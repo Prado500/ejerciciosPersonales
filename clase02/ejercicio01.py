45ecf1573e8d7249c3d6202bee883acdb9e73fd2
Prado500 <daldeosmail@gmail.com>
2025-08-07 21:33:09 -0500

    ejercicio con buenas practicas aplicadas al momento de definir los argumentos de la funcion y levantar las excepciones del metodo registrarEmpleado. Tambien se implemento el try except para capturar e imprimir la excepcion.

--- clase02/ejercicio01.py
+++ clase02/ejercicio01.py
@@ -1,19 +1,25 @@
-def registrarEmpleado (nombre, cargo, salario):
-    if not nombre or not cargo or not salario or salario <=0 :
-        raise ValueError ("Datos no validos. Asegúrese que el nombre, el cargo y el salario fueron digitados.\nAsegúrese también que el valor del salario es un numero positivo diferente de 0. ")
-    return(nombre.title(), cargo.title(), float(salario))
+# def registrarEmpleado (nombre, cargo, salario):
+#     if not nombre or not cargo or not salario or salario <=0 :
+#         raise ValueError ("Datos no validos. Asegúrese que el nombre, el cargo y el salario fueron digitados.\nAsegúrese también que el valor del salario es un numero positivo diferente de 0. ")
+#     return(nombre.title(), cargo.title(), float(salario))
 
 # def mostrarEmpleados(empleados):
 #     for i, emp in enumerate(empleados, start = 1):
 #         print(f"{i}. {nombre} - {cargo} - ${salario:.2f}")
         
 
+def registrarEmpleado (nombre: str, cargo: str, salario):
+    if not isinstance(nombre, str) or not isinstance(cargo, str):
+        raise ValueError("El nombre y el cargo solo pueden contener letras")
+    if not nombre or not cargo or salario <= 0 or not salario or not isinstance(salario, int or float): # OJO! Si pongo or or not isinstance(salario, int) or not isinstance(salario, float), vuelvo la condicion mutuamente excluyente y nunca va a guardar el dato
+        raise ValueError("Ingrese por favor cada uno de los datos de los empleados: nombre, cargo y salario.\nRecuerde que el valor del salario debe ser un numero mayor que 0 y positivo.")
+    return (nombre.title(), cargo.title(), salario)
+
 def mostrarEmpleados(empleados):
     for i, (nombre, cargo, salario) in enumerate(empleados, start = 1):
         print(f"{i}. {nombre}, {cargo}, ${salario:.2f}")
         
 def main():
-    
     empleados = []
     
     try:
